d64bf3bec641a4b032e41f7f8b62d13875acaac5
Nicholas Kalpakis <nicholas.kalpakis@qvc.com>
2025-06-07 09:35:56 -0400

    Fix imports.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,21 @@
 import { LandingHeader } from "@/components/landing/landing-header"
 import { LandingFooter } from "@/components/landing/landing-footer"
-import Dashboard from "../dashboard"
+import { LandingCta } from "@/components/landing/landing-cta"
+import { LandingFeatures } from "@/components/landing/landing-features"
+import { LandingHero } from "@/components/landing/landing-hero"
+import { LandingHowItWorks } from "@/components/landing/landing-how-it-works"
+import { LandingTestimonials } from "@/components/landing/landing-testimonials"
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <LandingHeader />
       <main className="flex-1">
-        <Dashboard />
-        {/* <LandingHero />
+        <LandingHero />
         <LandingFeatures />
         <LandingHowItWorks />
         <LandingTestimonials />
-        <LandingCta /> */}
+        <LandingCta />
       </main>
       <LandingFooter />
     </div>
